cc4c2e8a20eb8c6e1f50e1c0ff18829ad734f6f5
Ricard Forniol Agustí <ricard.forniol@pushtech.com>
2015-01-30 11:42:05 +0100

    Build WokersLoader class methods

--- lib/workers_loader.rb
+++ lib/workers_loader.rb
@@ -10,5 +10,33 @@ module WorkersLoader
   mattr_accessor :workers
   @@workers = []
 
+  class << self
+    def add_path(path)
+      fail "Directory not found: `#{path}`" unless Dir.exist?(path)
+      @@workers_paths << path
+    end
+
+    def find(path)
+      path = Path.new(path)
+      path.files.map { |file| path.queue_for(file) }
+        .reject(&:blank?)
+    end
+
+    def load_workers!
+      workers_paths.each do |path|
+        workers_in_path = find(path)
+        next if workers_in_path.empty?
+
+        duplacates = workers_in_path
+          .select { |worker| workers.include?(worker) }
+        if duplacates.any?
+          fail("Workers already present! #{duplacates.join(', ')}")
+        end
+
+        self.workers += workers_in_path
+      end
+    end
+  end
+
   autoload :Path, 'workers_loader/path'
 end
---
+++ spec/lib/workers_loader_spec.rb
@@ -0,0 +1,46 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe WorkersLoader do
+  before do
+    described_class.workers_paths = []
+    described_class.workers = []
+  end
+  let(:workers_path) do
+    File.join(File.dirname(__FILE__), '..', 'support', 'dummy')
+  end
+  subject { described_class.workers_paths }
+
+  it { expect(subject).to eq([]) }
+
+  describe '::add_path' do
+    before { described_class.add_path(workers_path) }
+
+    it { expect(described_class.workers_paths).to eq([workers_path]) }
+
+    context 'dubplicate worker' do
+      let(:message) { 'Directory not found: `foo`' }
+      it { expect { described_class.add_path('foo') }.to raise_error(message) }
+    end
+  end
+
+  describe '::find' do
+    subject { described_class.find(workers_path) }
+
+    it { expect(subject.sort).to eq([:dummy_foo, :baz_queue].sort) }
+  end
+
+  describe '::load_workers!' do
+    before do
+      described_class.add_path(workers_path)
+      described_class.load_workers!
+    end
+
+    it { expect(described_class.workers).to eq([:baz_queue, :dummy_foo]) }
+
+    context 'prevent duplicates' do
+      let(:message) { 'Workers already present! baz_queue, dummy_foo' }
+      it { expect { described_class.load_workers! }.to raise_error(message) }
+    end
+  end
+end
